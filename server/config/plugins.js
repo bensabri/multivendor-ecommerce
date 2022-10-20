0105aa1d6a967dc5cfec1764bc30b4aed9f0d579
bensabri <ssabril@ymail.com>
2022-10-20 08:56:28 +0100

    update strapi to 4.4.5

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -1,32 +1,14 @@
-module.exports = ({ env }) => (
-  {
-    email: {
-      config: {
-        provider: "sendgrid",
-        providerOptions: {
-          apiKey: env("SENDGRID_API_KEY"),
-        },
-        settings: {
-          defaultFrom: "no-reply@medinah-store.com",
-          defaultReplyTo: "no-reply@medinah-store.com",
-        },
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "no-reply@medinah-store.com",
+        defaultReplyTo: "no-reply@medinah-store.com",
       },
     },
   },
-  {
-    upload: {
-      config: {
-        provider: "cloudinary",
-        providerOptions: {
-          cloud_name: env("CLOUDINARY_NAME"),
-          api_key: env("CLOUDINARY_KEY"),
-          api_secret: env("CLOUDINARY_SECRET"),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
-    },
-  }
-);
+});
